6cef60b1d062d9b798c2d98d3c513cddcc19acaf
Ferenc Baráth <ferenc.barath@stud.uni-corvinus.hu>
2022-09-30 10:45:35 +0200

    hello there

--- VerkoForm/VerkoForm/Form1.cs
+++ VerkoForm/VerkoForm/Form1.cs
@@ -7,17 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VerkoForm.Entities;
 
 namespace VerkoForm
 {
     public partial class Form1 : Form
     {
+        BindingList<user> users = new BindingList<user>();
         public Form1()
         {
             InitializeComponent();
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new user()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
