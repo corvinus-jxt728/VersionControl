05fac1fa0ed2077ff4f36e10227d7b5146e3b04e
Ferenc Baráth <ferenc.barath@stud.uni-corvinus.hu>
2022-09-30 11:02:50 +0200

    szziaaaPC

--- VerkoForm/VerkoForm/Resource1.Designer.cs
+++ VerkoForm/VerkoForm/Resource1.Designer.cs
@@ -70,7 +70,7 @@ namespace VerkoForm {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Keresztnév.
+        ///   Looks up a localized string similar to Utónév.
         /// </summary>
         internal static string FirstName {
             get {
