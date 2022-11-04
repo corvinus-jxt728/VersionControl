729f71489fbbdc5d4df20707630e69f83556d553
Ferenc Baráth <ferenc.barath@stud.uni-corvinus.hu>
2022-11-04 11:12:37 +0100

    ahalaó

---
+++ mikulasgyar/mikulasgyar/Abstractions/IToyFactory.cs
@@ -0,0 +1,14 @@
+﻿using mikulasgyar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mikulasgyar.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
--- mikulasgyar/mikulasgyar/Entities/BallFactory.cs
+++ mikulasgyar/mikulasgyar/Entities/BallFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mikulasgyar.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,10 @@ using System.Threading.Tasks;
 
 namespace mikulasgyar.Entities
 {
-    public class BallFactory
+    public class BallFactory : IToyFactory
     {
-        public Ball CreateNew()
+
+        public Toy CreateNew()
         {
             return new Ball();
         }
